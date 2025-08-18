f64fcac667ad316f7bc15e21bb3744e768fbcc2d
unknown <414-18@LAPTOP-RS5KIG51.szao.obr.mos.ru>
2025-08-18 13:01:46 +0300

    Add README.md

---
+++ motors_control.py
@@ -0,0 +1,13 @@
+from gpiozero import Robot
+import time
+time.sleep(20)
+robby = Robot
+left=(7,8)
+right=(9,10)
+time.sleep(1)
+for _ in range(80):
+    robby.forward()
+    time.sleep(1)
+    robby.right()
+    time.sleep(0.5)
+robby.stop()
